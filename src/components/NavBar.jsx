7a25210c3f7ae397c3ef61dd81dd56900e4e4aba
jhanhrndz <jhancarlosh134@gmail.com>
2025-02-20 15:45:01 -0500

    Adjusting proportions of the NavBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -27,18 +27,18 @@ const Navbar = () => {
 
   return (
     <nav
-      className={`fixed top-6 left-1/2 transform -translate-x-1/2 w-[80%] md:w-[50%] lg:w-[40%] px-6 py-3 rounded-2xl transition-all duration-300 z-50 ${
-        scrolling ?  "bg-white shadow-lg" : "bg-white/80 backdrop-blur-md"
+      className={`fixed top-4 left-1/2 transform -translate-x-1/2 w-[90%] md:w-[40%] lg:w-[35%] px-4 py-2 rounded-xl transition-all duration-300 z-50 ${
+        scrolling ? "bg-white shadow-lg" : "bg-white/80 backdrop-blur-md"
       }`}
     >
-      <ul className="flex justify-center space-x-6">
+      <ul className="flex justify-center space-x-4">
         {["Sobre mí", "Proyectos", "Habilidades", "Contacto"].map((section) => {
           const sectionId = section.toLowerCase().replace(" ", "-");
           return (
             <li key={section}>
               <a
                 href={`#${sectionId}`}
-                className={`text-lg font-medium transition-colors ${
+                className={`text-sm font-medium transition-colors ${
                   activeSection === sectionId
                     ? "text-blue-500"
                     : "text-gray-700 hover:text-blue-500"
